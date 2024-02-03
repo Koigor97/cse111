0e8f30ec88f322011f6a313ea4ecbf6f7a426fa0
Koigor97 <turaysamuel101@yahoo.com>
2024-02-03 16:00:26 +0200

    create basic class

---
+++ oop/playerCharacter.py
@@ -0,0 +1,10 @@
+
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def player_stat(self):
+        print(f"Name: {self.name}\nAge: {self.age}")
+
+
