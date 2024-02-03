4e62f6ed36cd3d3199580c46bf2598b454be52c4
Koigor97 <turaysamuel101@yahoo.com>
2024-02-03 19:22:00 +0200

    add static method

--- refresher/playerCharacter.py
+++ refresher/playerCharacter.py
@@ -7,4 +7,6 @@ class PlayerCharacter:
     def player_stat(self):
         print(f"Name: {self.name}\nAge: {self.age}")
 
-
+    @staticmethod
+    def capitalize_key_and_array_value(param):
+        return {key.capitalize() :  [value] for key, value in param.items()}
